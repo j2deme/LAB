5eae43d6493067f439876f5cd0f439b579b1d8b9
j2deme <j2deme@gmail.com>
2019-07-22 12:06:22 -0500

    Resuelve conflicto ApexCharts con Vue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,7 +82,7 @@
   </div>
   <!-- Scripts -->
   @yield('js')
-  <script src="{{ asset('js/app.js') }}" defer></script>
+  {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 </body>
 
 </html>
